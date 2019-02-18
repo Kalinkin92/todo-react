5d857442d140ff791e3473272cae1bd3a8049eb8
Igor <kalinkin92@gmail.com>
2019-02-18 16:12:05 +0300

    first

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app-header.css';
+
+const AppHeader = ({toDo, done}) => {
+  return (
+    <div className="app-header d-flex">
+      <h1>Дела Артёма</h1>
+      <h2>{toDo} Всего, {done} сделано</h2>
+    </div>
+  );
+};
+
+export default AppHeader;
